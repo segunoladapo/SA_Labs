ab30685745bfa591d96a8df4f6e7de4e5164a6a5
segun <segxy4show2000@gmail.com>
2018-07-12 08:15:47 -0500

    Initial commit for lab12 assignment

---
+++ Lab12/security/service-c/src/main/java/edu/mum/sa/lab/servicec/servicec/ServiceCApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.sa.lab.servicec.servicec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCApplication.class, args);
+	}
+}
